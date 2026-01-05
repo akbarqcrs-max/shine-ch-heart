a696324aa372cb62c519ea54bdfdb4e2fffa28a7
akbar-99 <akbar-99@users.noreply.github.com>
2026-01-05 13:36:00 +0400

    Update Google Maps location to exact Thrissur coordinates

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -330,7 +330,7 @@ const Contact = () => {
                 className="rounded-2xl overflow-hidden shadow-lg h-64 bg-section-alt"
               >
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.1827874916447!2d76.21376637507652!3d10.5265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDMxJzM1LjQiTiA3NsKwMTInNTcuNiJF!5e0!3m2!1sen!2sin!4v1000000000000!5m2!1sen!2sin"
+                  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d244.82870532937438!2d76.20056915283203!3d10.608206748962402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDM2JzI5LjUiTiA3NsKwMTInMDIuMCJF!5e0!3m2!1sen!2sin!4v1704457200000!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
